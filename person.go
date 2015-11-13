50725ed312b8e5deefb6e512abaa7d3db77be094
serbe <mkoctuk@gmail.com>
2015-11-13 16:09:10 +0300

    more readable comments, change type og GetPerson return

--- person.go
+++ person.go
@@ -2,86 +2,87 @@ package cinemate
 
 import (
 	"encoding/xml"
-	"fmt"
 	"net/url"
 	"strconv"
 	"time"
 )
 
 // GetPerson Основная информация о персоне
-// example: http://api.cinemate.cc/person?id=3971&apikey=APIKEY&format=xml
-// apikey	ключ разработчика
-// id	ID персоны
-// format	необязательный параметр формата возвращаемых сервером данных: xml (по умолчанию) или json
-func (ccc *API) GetPerson(id int) ([]Person, error) {
+// Пример запроса: http://api.cinemate.cc/person?id=3971&apikey=APIKEY&format=xml
+// apikey ключ разработчика
+// id     ID персоны
+// format необязательный параметр формата возвращаемых сервером данных: xml (по умолчанию) или json
+func (api *API) GetPerson(id int) (person Person, err error) {
 	time.Sleep(1 * time.Second)
-	var result Response
+	var result APIResponse
 	var u url.URL
 	u.Scheme = "http"
 	u.Host = "api.cinemate.cc"
 	u.Path = "/person"
 	q := u.Query()
-	q.Set("apikey", ccc.apikey)
+	q.Set("apikey", api.apikey)
 	q.Set("id", strconv.Itoa(id))
 	q.Set("format", "xml")
 	u.RawQuery = q.Encode()
 	xmlBody, err := getXML(u.String())
 	if err != nil {
-		return []Person{}, err
+		return
 	}
 	err = xml.Unmarshal(xmlBody, &result)
-	return result.Person, err
+	person = result.Persons[0]
+	return
 }
 
 // GetPersonMovies Информация о персоне, включая фильмы, в съемке которых персона
 // принимала участие в качестве актера или режиссера. Основная информация о
 // персоне идентична команде person.
-// example: http://api.cinemate.cc/person.movies?id=3971&apikey=APIKEY&format=xml
-// apikey	ключ разработчика
-// id	ID персоны
-// format	необязательный параметр формата возвращаемых сервером данных: xml (по умолчанию) или json
-func (ccc *API) GetPersonMovies(id int) ([]Person, error) {
+// Пример запроса: http://api.cinemate.cc/person.movies?id=3971&apikey=APIKEY&format=xml
+// apikey ключ разработчика
+// id     ID персоны
+// format необязательный параметр формата возвращаемых сервером данных: xml (по умолчанию) или json
+func (api *API) GetPersonMovies(id int) (persons []Person, err error) {
 	time.Sleep(1 * time.Second)
-	var result Response
+	var result APIResponse
 	var u url.URL
 	u.Scheme = "http"
 	u.Host = "api.cinemate.cc"
 	u.Path = "/person.movies"
 	q := u.Query()
-	q.Set("apikey", ccc.apikey)
+	q.Set("apikey", api.apikey)
 	q.Set("id", strconv.Itoa(id))
 	q.Set("format", "xml")
 	u.RawQuery = q.Encode()
-	fmt.Println(u.String())
 	xmlBody, err := getXML(u.String())
 	if err != nil {
-		return []Person{}, err
+		return
 	}
 	err = xml.Unmarshal(xmlBody, &result)
-	return result.Person, err
+	persons = result.Persons
+	return
 }
 
 // GetPersonSearch Метод возвращает первые 10 результатов поиска по базе персон
-// example: http://api.cinemate.cc/person.search?apikey=APIKEY&term=гиленхол&format=xml
-// apikey	ключ разработчика
-// term	искомая строка; поддерживается уточняющий поиск по году выхода фильма (год должен быть указан в конце искомой строки, например, "Пираты кариб 2003") и коррекцию ошибок при печати
-// format	необязательный параметр формата возвращаемых сервером данных: xml (по умолчанию) или json
-func (ccc *API) GetPersonSearch(term string) ([]Person, error) {
+// Пример запроса: http://api.cinemate.cc/person.search?apikey=APIKEY&term=гиленхол&format=xml
+// apikey ключ разработчика
+// term   искомая строка; поддерживается уточняющий поиск по году выхода фильма (год должен быть указан в конце искомой строки, например, "Пираты кариб 2003") и коррекцию ошибок при печати
+// format необязательный параметр формата возвращаемых сервером данных: xml (по умолчанию) или json
+func (api *API) GetPersonSearch(term string) (persons []Person, err error) {
 	time.Sleep(1 * time.Second)
-	var result Response
+	var result APIResponse
 	var u url.URL
 	u.Scheme = "http"
 	u.Host = "api.cinemate.cc"
 	u.Path = "/person.search"
 	q := u.Query()
-	q.Set("apikey", ccc.apikey)
+	q.Set("apikey", api.apikey)
 	q.Set("term", term)
 	q.Set("format", "xml")
 	u.RawQuery = q.Encode()
 	xmlBody, err := getXML(u.String())
 	if err != nil {
-		return []Person{}, err
+		return
 	}
 	err = xml.Unmarshal(xmlBody, &result)
-	return result.Person, err
+	persons = result.Persons
+	return
 }
